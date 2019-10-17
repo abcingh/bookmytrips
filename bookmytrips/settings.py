33aedc2ac7e4cd491e864a1f032e0095985ca05e
Abhishek Singh <abhishek.singh@mountblue.io>
2019-10-17 10:22:26 +0530

    edit authentication <login>

--- bookmytrips/settings.py
+++ bookmytrips/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'main',
     'bootstrapform',
     'allauth',
     'allauth.account',
@@ -138,8 +137,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"), 
 ]
-
 LOGIN_REDIRECT_URL = '/'
