03800ca5d66ed14c221fe11a37fdc10161d73501
praashant.twr <prashant.tiwari@mountblue.io>
2019-10-17 12:22:23 +0530

    create home page

--- bookmytrips/urls.py
+++ bookmytrips/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
     path('accounts/', include('allauth.urls')),
+    path('', include('mainapp.urls')),
 
 ]
