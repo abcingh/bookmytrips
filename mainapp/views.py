c82133cd558ea943aa26393aa742d8d7c480626b
praashant.twr <prashant.tiwari@mountblue.io>
2019-10-18 10:42:34 +0530

    Add UI to Auth and Home page

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,8 +4,17 @@ from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.http import JsonResponse, HttpResponse
+from .forms import FindDestinationForm
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        form = FindDestinationForm(request.POST)
+        if form.is_valid():
+            place = form.cleaned_data['place']
+            print(place)
+        # return HttpResponse('Form Called')
+    form = FindDestinationForm()
+    return render(request, 'index.html', {'form': form})
